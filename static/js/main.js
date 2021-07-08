ef1e8be222c90ce84bb2f5adaadfaac66e710c29
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2021-07-08 08:09:28 +0300

    add table swither

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,29 @@
-const btn = document.getElementById("btn");
-const results = document.getElementById("results");
+// const btn = document.getElementById("btn");
+// const results = document.getElementById("results");
 
-btn.addEventListener("click", function(e){
-    e.preventDefault();
-    if (results.classList.contains("hide")) {
-        results.classList.remove("hide");
+// btn.addEventListener("click", function(e){
+//     e.preventDefault();
+//     if (results.classList.contains("hide")) {
+//         results.classList.remove("hide");
+//       }
+// });
+
+const procatTable = document.getElementById('procatTable');
+const zvarniTable = document.getElementById('zvarniTable');
+
+
+function changeTable(table){
+  if (table == "procat"){
+        if (procatTable.classList.contains("hide")) {
+          procatTable.classList.remove("hide");
+          zvarniTable.classList.add('hide');
       }
-});
+  }
+  if (table == "zvarni"){
+        if (zvarniTable.classList.contains("hide")) {
+          zvarniTable.classList.remove("hide");
+          procatTable.classList.add('hide');
+      }
+  }
+  
+}
