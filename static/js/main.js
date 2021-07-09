28a7689747da438ddfe4f88703cb80ade335708b
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2021-07-09 08:01:03 +0300

    add registration page

--- static/js/main.js
+++ static/js/main.js
@@ -10,6 +10,8 @@
 
 const procatTable = document.getElementById('procatTable');
 const zvarniTable = document.getElementById('zvarniTable');
+const procatSwith = document.getElementById('procat-swith');
+const zvarniSwith = document.getElementById('zvarni-swith');
 
 
 function changeTable(table){
@@ -17,12 +19,18 @@ function changeTable(table){
         if (procatTable.classList.contains("hide")) {
           procatTable.classList.remove("hide");
           zvarniTable.classList.add('hide');
+          zvarniSwith.classList.remove("active");
+          zvarniSwith.classList.add("not-active");
+          procatSwith.classList.add("active");
       }
   }
   if (table == "zvarni"){
         if (zvarniTable.classList.contains("hide")) {
           zvarniTable.classList.remove("hide");
           procatTable.classList.add('hide');
+          procatSwith.classList.remove("active");
+          procatSwith.classList.add("not-active");
+          zvarniSwith.classList.add("active");
       }
   }
   
