76568dff2271463676ba797b0875bcb33f6738cb
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2021-08-05 17:35:34 +0300

    add js logic to main page

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-
+d
 const procatTable = document.getElementById('procatTable');
 const zvarniTable = document.getElementById('zvarniTable');
 const procatSwith = document.getElementById('procat-swith');
@@ -27,15 +27,77 @@ function changeTable(table){
   
 }
 
-function show(name){
-  let someField = document.getElementById(name);
-  someField.classList.remove("hide");
+function show(changeNameDiv, currentName,  currentNameLine){
+  let newField = document.getElementById(changeNameDiv);
+  let name = document.getElementById(currentName).innerText;
+  let nameLine = document.getElementById(currentNameLine);
+  nameLine.classList.add("hide");
+  newField.classList.remove("hide");
+  newField.firstElementChild.setAttribute('placeholder',name); 
 }
 
-function hide(name){
-  let someField = document.getElementById(name);
-  someField.classList.add("hide");
+function hide(changeNameDiv, currentName,  currentNameLine, nameInput){
+  let newField = document.getElementById(changeNameDiv);
+  let nameLine = document.getElementById(currentNameLine);
+  let newName = document.getElementById(nameInput).value;
+
+  if (newName){
+    document.getElementById(currentName).innerText = newName;
+  }
+
+  newField.classList.add("hide");
+  nameLine.classList.remove("hide");
 }
+
+
+function showGroups(groups, groupsName){
+  let groupsArr = document.getElementById(groups).innerText.split(',');
+  let insertHTML = '';
+  for (let element of groupsArr){
+    insertHTML += `<div><input type="text" class="input constrGroup" placeholder=${element}></div>`;
+  }
+  insertHTML += `<div id='groupBtn'><a href="#" class="btn-orange" onclick="hideGroups('constrGroup', 'groups', 'groupsNames')">Зберегти</a><div>`;
+  document.getElementById(groupsName).parentElement.insertAdjacentHTML('afterend',insertHTML);
+  document.getElementById(groupsName).classList.add("hide");
+}
+
+function hideGroups(constrGroup, groups, groupsNamesLine){
+  let groupsArr = document.getElementsByClassName(constrGroup);
+  let groupsNames = [];
+
+  for (let element of groupsArr){
+    if (element.value){
+      groupsNames.push(element.value);
+    } else {
+      groupsNames.push(element.placeholder);
+    }
+  }
+
+  while(groupsArr.length > 0){
+    groupsArr[0].parentNode.remove();
+  }
+
+  document.getElementById('groupBtn').remove();
+
+
+  let groupsSpan = '';
+
+  for (let elementName of groupsNames){
+    groupsSpan += elementName + ', ';
+  }
+
+  groupsSpan  = groupsSpan.substring(0, groupsSpan .length - 2);
+
+  let groupsLine = document.getElementById(groups);
+  groupsLine.innerText = groupsSpan;
+  document.getElementById(groupsNamesLine).classList.remove("hide");
+
+
+
+}
+
+
+
 
 function copyLine(element){
   let row = element.parentElement.parentElement;
